a98735e36334810d9ee569990fe20d65249cf5be
nevroje <joakim.berglind@gmail.com>
2019-01-22 11:45:50 +0100

    Fixed spelling

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -2,10 +2,10 @@ require './lib/atm.rb'
 
 describe ATM do
 
-    it 'has 1000$ on initialize'    do
+    it 'has 1000$ on initialize'   do
         expect(subject.funds).to eq 1000
 
-  end
+    end
     it 'funds are reduced at withdraw'  do
         subject.withdraw(50)
         expect(subject.funds).to eq 950
