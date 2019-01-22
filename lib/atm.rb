9f9439d412620efd5bebd3c6771d056863923d91
pilar_washburn <pw325555@gmail.com>
2019-01-22 11:42:05 +0100

    fourth commit

--- lib/atm.rb
+++ lib/atm.rb
@@ -4,4 +4,28 @@ class ATM
     def initialize
         @funds = 1000
     end
+
+    def withdraw(amount)
+        @funds -= amount
+    end  
+    
 end
+
+    def withdraw(amount, account)
+        case
+        when amount > account.balance
+
+        return
+        else
+
+            @funds -= amount
+
+            account.balance = account.balance - amount
+
+            {status: true, message: 'success', date: Date.today, amount: amount}
+
+        end
+    end
+
+end
+
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,13 +1,33 @@
 require './lib/atm.rb'
 
 describe ATM do
+    let(:account) { instance_double('Account') }
 
-    it 'has 1000$ on initialize'    do
+    before do
+
+        allow(account).to receive(:balance).and_return(100)
+
+        allow(account).to receive(:balance=)
+    end
+
+    it 'allows withdrawal if account has enough balance.' do
+
+        expected_output = {status: true, message: 'success', date: Date.today, amount: 45 }
+
+        expect(subject.withdraw(45, account)).to eq expected_output
+    end
+    
+    
+        it 'has 1000$ on initialize' do 
         expect(subject.funds).to eq 1000
+    end
 
-  end
     it 'funds are reduced at withdraw'  do
         subject.withdraw(50)
         expect(subject.funds).to eq 950
     end
+
+
+
+
 end
