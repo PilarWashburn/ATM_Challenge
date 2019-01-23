a61750bf4e05f74b1689995ae61e93f045a26155
nevroje <joakim.berglind@gmail.com>
2019-01-23 11:50:46 +0100

    Changed dates to match Date.today 7 green 0 red.

--- lib/atm.rb
+++ lib/atm.rb
@@ -12,7 +12,9 @@ class Atm
         when insufficient_funds_in_Atm?(amount)
             { status: false, message: 'insufficient funds in Atm', date: Date.today }
         when incorrect_pin?(pin_code, account.pin_code)
-            { status: false, message: 'wrong pin', date: Date.today }     
+            { status: false, message: 'wrong pin', date: Date.today }  
+        when card_expired?(account.exp_date)
+            { status: false, message: 'card expired', date: Date.today }   
         else
             perform_transaction(amount, pin_code, account)
         end
@@ -37,6 +39,10 @@ class Atm
     
     def incorrect_pin?(pin_code, actual_pin)
         pin_code != actual_pin
+    end
+
+    def card_expired?(exp_date)
+        Date.strptime(exp_date, '%m/%y') < Date.today
     end
 end
 
