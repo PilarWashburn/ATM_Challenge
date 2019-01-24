a85119969c16019eceeb9cec7160cb764bc93d46
nevroje <joakim.berglind@gmail.com>
2019-01-24 20:31:54 +0100

    Added relevant code for account iowner in account_spec.rb file

--- lib/account.rb
+++ lib/account.rb
@@ -4,14 +4,20 @@ class Account
     attr_accessor :balance, :pin_code, :account_status, :exp_date
 
     def initialize(account_status, balance, pin_code, exp_date)
-        @expiry_date = '%m/%y'
+        @expiry_date = '24/24'
         @account_status = :active
         @balance = 1000
-        @pin_code = '1234'
+        @pin_code = 1234
     end
 
+    private
+
     def set_expire_date
-        Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime('%m/%Y')
+        Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%Y')
+    end
+
+    def deactivate
+        @account_status = :deactivated
     end
 
 end
