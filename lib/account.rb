3247beb996ee7426bb59cefea57132981bf38c8f
nevroje <joakim.berglind@gmail.com>
2019-01-24 20:42:18 +0100

    added owner code to account.rb, not working yet

--- lib/account.rb
+++ lib/account.rb
@@ -4,10 +4,11 @@ class Account
     attr_accessor :balance, :pin_code, :account_status, :exp_date
 
     def initialize(account_status, balance, pin_code, exp_date)
-        @expiry_date = '24/24'
+        @expiry_date = '%m/%y'
         @account_status = :active
         @balance = 1000
         @pin_code = 1234
+        set_owner =
     end
 
     private
@@ -18,6 +19,10 @@ class Account
 
     def deactivate
         @account_status = :deactivated
+    end
+
+    def set_owner(obj)
+        obj == nil ?  missing_owner : @owner = obj
     end
 
 end
