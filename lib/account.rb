eaae9b2bef5f4db7b8a07973c8f2372781539766
pilar_washburn <pw325555@gmail.com>
2019-01-24 16:48:16 +0100

    still troubleshooting Account

--- lib/account.rb
+++ lib/account.rb
@@ -2,31 +2,16 @@ class Account
     STANDARD_VALIDITY_YRS = 5
     attr_accessor :balance, :pin_code, :account_status, :exp_date
 
-    def initialize
-        @account_status = active
-    end
-
-    def account(pin_code, balance, account_status, exp_date)
-
-    end
-
 def initialize (account_status, pin_code, balance, expire_date)
     @account_status = :active
     @pin_code = 1234
     @balance = 950
-    @expire_date = '04/23'
+    @expire_date = '01/24'
 end
 
-    def deactivate
-        @account_status = :deactivated
-    end
+def set_expire_date
+    Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime('%m/%Y')
+end
 
-    def incorrect_pin?(pin_code, actual_pin)
-        pin_code != actual_pin    
-    end
-        
-    def set_expire_date
-        Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%Y')
-    end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,10 +4,6 @@ require 'date'
 describe Account do
     let(:person) {instance_double('Person', name: 'Thomas')}
 
-
-    it 'is expected to have a balance' do
-        expected_balance => 0
-        expect(balance).to eq 950
     
     it 'is expected to have an expiry date on initialize' do
         expected_date = Date.today.next_year(5).strftime("%m/%y")
@@ -15,7 +11,4 @@ describe Account do
     end
 
     
-
-    
-
 end
