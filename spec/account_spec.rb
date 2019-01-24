bbba361f0731ffd14cc775bafc3264cae973d7cc
pilar_washburn <pw325555@gmail.com>
2019-01-24 13:46:43 +0100

    troubleshooting account class

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -30,4 +30,11 @@ describe Account do
         expected_date = Date.today.next_year(5).strftime('%m/%y')
         expect(subject.exp_date).to eq expected_date
     end
+
+    it 'check length of a number' do
+        number = 1234
+        number_length = Math.log10(number).to_i + 1
+        expect(number_length).to eq 4
+    end
+
 end
