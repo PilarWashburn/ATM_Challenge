1c456a05a7de8466cdf46438058da62622ef9970
nevroje <joakim.berglind@gmail.com>
2019-01-24 15:46:24 +0100

    Added code to account files

--- lib/account.rb
+++ lib/account.rb
@@ -1,45 +1,8 @@
 class Account
-
-    #attr_accessor :account
     STANDARD_VALIDITY_YRS = 5
 
-    def withdraw(amount, pin_code, account)
-        case
-            when incorrect_pin?(pin_code, account.pin_code)
-                {status: false, message: 'wrong pin', date: Date.today}  
-                    when card_expired?(account.exp_date)
-                        {status: false, message: 'card expired', date: Date.today}
-                            when disabled_account?(account.account_status)
-                        {status: false, message: 'disabled account', date: Date.today}
-                    when insufficient_funds_in_account?(amount, pin_code, account)
-                {status: false, message: 'insufficient funds in account', date: Date.today}
-            else
-        perform_transaction(amount, pin_code, account)
-    end
-    
-    end
-
-    private
-
-    def incorrect_pin?(pin_code, actual_pin)
-        pin_code != actual_pin
-    end
-
-    def card_expired?(exp_date)
-        Date.strptime(exp_date, '%m/%y') < Date.today
-    end
-
-    def disabled_account?(account_status)
-        account_status == :disabled
-    end
-
-    def insufficient_funds_in_account?(amount, pin_code, account)
-        amount > account.balance
-    end
-
     def set_expire_date
-        Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strptime('%m/%y')
+        Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime('%m/%Y')
     end
-
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,50 +1,11 @@
-require './lib/account.rb'
-#require './'
+# require './lib/account.rb'
+# require 'date'
 
 describe Account do
-
-    let(:account) {instance_double('Account', pin_code: '1234', exp_date: '04/23', account_status: :active)}
-    
-
-    it 'reject withdraw if pin is wrong'  do
-        expected_output = {status: false, message: 'wrong pin', date: Date.today}
-        expect(subject.withdraw(50, 9999, account)).to eq expected_output
-    end
-
-    it 'reject withdraw if card is expired'  do
-        allow(account).to receive(:exp_date).and_return('04/18')
-        expected_output = {status: false, message: 'card expired', date: Date.today}
-        expect(subject.withdraw(6, '1234', account)).to eq expected_output
-    end
-
-    it 'reject withdraw if account is disabled'  do
-        allow(account).to receive(:account_status).and_return(:disabled)
-        expected_output = {status: false, message: 'disabled account', date: Date.today}
-        expect(subject.withdraw(6, '1234', account)).to eq expected_output
-    end
-
-    it 'reject withdrawal if account has insufficient funds'  do
-        expect_output = {status: false, message: 'insufficient funds in account', date: Date.today}
-        expect(subject.withdraw(105, '1234', account)).to eq expect_output
-    end
+    let(:account) {instance_double('Account', pin_code: '1234', exp_date: '04/23', account_status: :active, :balance)}
 
     it 'is expected to have an expiry date on initialize' do
-        expected_date = Date.today.next_year(5).strftime('%m/%y')
+        expected_date = Date.today.next_year(5).strftime("%m/%y")
         expect(subject.exp_date).to eq expected_date
-    end
-
-    it 'checks length of a number' do
-        number = 1234
-        number_length = Math.log10(number).to_i + 1
-        expect(number_length).to eq 4
-    end
-
-    it 'is expected to have :active status on initialize' do
-        expect(subject.account_status).to eq :active
-    end
-
-    it 'deactivates account using Instance method' do
-        subject.deactivate
-        expect(subject.account_status).to eq :deactivated
     end
 end
