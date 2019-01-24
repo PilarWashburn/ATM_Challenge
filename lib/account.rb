3c819a2f465167b222c8e690e47e8604aab49929
nevroje <joakim.berglind@gmail.com>
2019-01-24 16:46:43 +0100

    Added initialize code to account.rb

--- lib/account.rb
+++ lib/account.rb
@@ -1,17 +1,17 @@
 class Account
     STANDARD_VALIDITY_YRS = 5
+
     attr_accessor :balance, :pin_code, :account_status, :exp_date
 
-    def initialize
-        @account_status = active
-    end
-
-    def account(pin_code, balance, account_status, exp_date)
-
+    def initialize(account_status, balance, pin_code, exp_date)
+        @expire_date = expire_date
+        @account_status = account_status
+        @balance = balance
+        @pin_code = pin_code
     end
 
     def set_expire_date
-        Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%Y')
+        Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime('%m/%Y')
     end
 
 end
