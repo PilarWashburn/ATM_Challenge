8cea46fbc84b78e8f9da1fac48cd17d3b78b9343
nevroje <joakim.berglind@gmail.com>
2019-01-25 10:05:59 +0100

    removed commented out code

--- lib/account.rb
+++ lib/account.rb
@@ -4,7 +4,7 @@ class Account
 
     attr_accessor :balance, :pin_code, :account_status, :exp_date
 
-    def initialize(account_status, balance, pin_code, exp_date)
+    def initialize
         @expiry_date = set_exp_date
         @account_status = :active
         @balance = account.balance
@@ -16,21 +16,4 @@ class Account
     def incorrect_pin?(pin_code, actual_pin)
         pin_code != actual_pin
     end
-
-    # def set_exp_date
-    #     Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime('%m/%y')
-    # end
-
-    # def deactivate
-    #     @account_status = :deactivated
-    # end
-
-    # def set_owner(obj)
-    #     obj == nil ?  missing_owner : @owner = obj
-    # end
-
-
-    # def insufficient_funds_in_account?(amount, pin_code, account)
-    #     amount > account.balance
-    # end
 end
