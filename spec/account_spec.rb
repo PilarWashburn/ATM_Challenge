d915c2f2e6bca2ab2d4d53a9b57ffd7bccf18613
pilar_washburn <pw325555@gmail.com>
2019-01-25 10:44:30 +0100

    pin_code working - tests green

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,6 +4,7 @@ require 'date'
 describe Account do
 
     it 'check length of a number' do
+        pin_code = rand(1000..9999)
         number_length = Math.log10(pin_code).to_i + 1
         expect(number_length).to eq 4
     end
