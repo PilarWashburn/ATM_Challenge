10f5ced4b67f369b4a4ef5441fd67bf6e55f91e2
nevroje <joakim.berglind@gmail.com>
2019-01-25 16:04:47 +0100

    Readme changes

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -31,5 +31,5 @@ describe Account do
 
     it 'is expected to raise an error if no owner is set' do
         expect {described_class.new}.to raise_error 'An account owner is required'
-    end
+    end 
 end
