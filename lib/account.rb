261efc12adae4c9434aa3b9f8f73545b0fe6b48a
nevroje <joakim.berglind@gmail.com>
2019-01-25 09:56:35 +0100

    Fixing merges and code

--- lib/account.rb
+++ lib/account.rb
@@ -5,30 +5,32 @@ class Account
     attr_accessor :balance, :pin_code, :account_status, :exp_date
 
     def initialize(account_status, balance, pin_code, exp_date)
-        @expiry_date = exp_date
+        @expiry_date = set_exp_date
         @account_status = :active
-        @balance = :balance
+        @balance = account.balance
         @pin_code = rand(1000..9999)
     end
 
     private
 
-    def set_exp_date
-        Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime('%m/%y')
+    def incorrect_pin?(pin_code, actual_pin)
+        pin_code != actual_pin
     end
 
-    def deactivate
-        @account_status = :deactivated
-    end
+    # def set_exp_date
+    #     Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime('%m/%y')
+    # end
+
+    # def deactivate
+    #     @account_status = :deactivated
+    # end
 
     # def set_owner(obj)
     #     obj == nil ?  missing_owner : @owner = obj
     # end
 
-    def incorrect_pin?(pin_code, actual_pin)
-        pin_code != actual_pin
-    end
 
-    def insufficient_funds_in_account?(amount, pin_code, account)
-        amount > account.balance
+    # def insufficient_funds_in_account?(amount, pin_code, account)
+    #     amount > account.balance
+    # end
 end
